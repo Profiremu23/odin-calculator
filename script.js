14b632779c5cf6d9c8d68866642f162f386c9b9b
Profiremu23 <profirdani23@gmail.com>
2023-09-09 18:44:06 +0200

    Added functional operation logic for console

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+// Initializing variables for calculator display
+let firstNumber = 3;
+let operator = "+";
+let secondNumber = 9;
+
+// Functions for various operators
 function add(x, y) {
     return x + y;
 }
@@ -12,4 +18,19 @@ function multiply(x, y) {
 
 function divide(x, y) {
     return x / y;
+}
+
+// Operate initialization logic
+function operate(firstNumber, operator, secondNumber) {
+    if (operator == "+") {
+        return add(firstNumber, secondNumber);
+    } else if (operator == "-") {
+        return subtract(firstNumber, secondNumber);
+    } else if (operator == "*") {
+        return multiply(firstNumber, secondNumber);
+    } else if (operator == "/") {
+        return divide(firstNumber, secondNumber);
+    } else {
+        alert("Error, something went wrong while calculating!")
+    }
 }
